fd63a35cdbfa190d27152fc6b8b09c3f6a631a9b
Roger Fernandes <roger.afernandes@hotmail.com>
2021-11-03 08:26:56 -0300

    feat: add users module

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,37 @@
+import { hash } from "bcrypt";
+import { inject, injectable } from "tsyringe";
+
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { IUserRepository } from "../../repositories/IUsersRepository";
+
+@injectable()
+class CreateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUserRepository
+  ) {}
+
+  async execute({
+    name,
+    password,
+    email,
+    driver_license,
+  }: ICreateUserDTO): Promise<void> {
+    const userAlredyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlredyExists) {
+      throw new Error("User alredy exists");
+    }
+
+    const passwordHash = await hash(password, 8);
+
+    await this.usersRepository.create({
+      name,
+      password: passwordHash,
+      email,
+      driver_license,
+    });
+  }
+}
+
+export { CreateUserUseCase };
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,5 +1,7 @@
 import { container } from "tsyringe";
 
+import { UsersRepository } from "../../modules/accounts/repositories/implementations/UserRepository";
+import { IUserRepository } from "../../modules/accounts/repositories/IUsersRepository";
 import { ICategoryRepository } from "../../modules/cars/repositories/ICategoriesRepository";
 import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository";
 import { SpecificationRepostirory } from "../../modules/cars/repositories/implementations/SpecificationRepostirory";
@@ -13,4 +15,9 @@ container.registerSingleton<ICategoryRepository>(
 container.registerSingleton<ISpecificationRepository>(
   "SpecificationRepostirory",
   SpecificationRepostirory
+);
+
+container.registerSingleton<IUserRepository>(
+  "UsersRepository",
+  UsersRepository
 );
