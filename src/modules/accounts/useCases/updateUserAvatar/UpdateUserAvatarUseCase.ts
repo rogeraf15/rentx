ade4309f9d32e688e9c3fa922c3d32dd0fe39876
Roger Fernandes <roger.afernandes@hotmail.com>
2021-11-05 10:07:41 -0300

    feat: add user avatar

---
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -0,0 +1,32 @@
+import { inject, injectable } from "tsyringe";
+
+import { deleteFile } from "../../../../utils/file";
+import { IUserRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  user_id: string;
+  avatar_file: string;
+}
+
+@injectable()
+class UpdateUserAvatarUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUserRepository
+  ) {}
+
+  async execute({ avatar_file, user_id }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findById(user_id);
+    const oldAvatar = user.avatar;
+
+    if (user.avatar) {
+      await deleteFile(`./tmp/avatar/${oldAvatar}`);
+    }
+
+    user.avatar = avatar_file;
+
+    await this.usersRepository.create(user);
+  }
+}
+
+export { UpdateUserAvatarUseCase };
