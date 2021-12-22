9b44b756a4d1a38a154f3486230df10b4e9b6bd4
Roger Fernandes <roger.afernandes@hotmail.com>
2021-12-22 12:14:57 -0300

    fix: fixed typeorm database

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,12 +1,11 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-export default async (host = "database_ignite"): Promise<Connection> => {
+export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
   const DATABASE_TEST = "rentx_test";
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === "test" ? "localhost" : host,
       database:
         process.env.NODE_ENV === "test"
           ? DATABASE_TEST
