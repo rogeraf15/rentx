a4e2f75867ccf0689fbc2c8fc52d2742b82e1465
Roger Fernandes <roger.afernandes@hotmail.com>
2021-12-23 22:48:15 -0300

    feat: add rate limiter

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -6,6 +6,7 @@ import swaggerUi from "swagger-ui-express";
 
 import upload from "@config/upload";
 import { AppError } from "@shared/errors/AppErrors";
+import rateLimiter from "@shared/infra/http/middlewares/rateLimiter";
 import createConnection from "@shared/infra/typeorm";
 
 // eslint-disable-next-line import-helpers/order-imports
@@ -16,6 +17,8 @@ import { router } from "./routes";
 
 createConnection();
 const app = express();
+
+app.use(rateLimiter);
 
 app.use(express.json());
 
